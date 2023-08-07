a41ef75bf51ea51524b714290f4cd43dc77b99d6
htfc786 <2721672639@qq.com>
2023-08-07 16:36:56 +0800

    add: 历史记录功能添加

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -49,11 +49,14 @@
 import SqlEditor from "../components/SqlEditor.vue";
 import QuestionBoard from "../components/QuestionBoard.vue";
 import SqlResult from "../components/SqlResult.vue";
-import { computed, ref, watch } from "vue";
+import { computed, ref, watch, onMounted } from "vue";
 import { QueryExecResult } from "sql.js";
 import { allLevels, getLevelByKey } from "../levels";
 import { checkResult } from "../core/result";
 import CodeEditor from "../components/CodeEditor.vue";
+import { message } from "ant-design-vue";
+import { readLevel, saveLevel } from "../core/saveLevel";
+import { useRouter } from "vue-router";
 
 interface IndexPageProps {
   levelKey?: string;
@@ -73,11 +76,53 @@ const errorMsgRef = ref<string>();
 const resultStatus = ref<number>(-1);
 const defaultActiveKeys = ["result"];
 const activeKeys = ref([...defaultActiveKeys]);
+const router = useRouter();
+
+// 加载缓存关卡
+const loadSaveLevelKey = () => {
+  const saveLevelKey = readLevel();
+  if (!saveLevelKey) {
+    return;
+  }
+  // 跳转
+  router.push(`/learn/${saveLevelKey}`);
+  // 提示信息
+  const levelInfo = getLevelByKey(saveLevelKey);
+  message.info(`您上次学到 ${levelInfo.title}，已自动为您跳转`);
+};
+
+/**
+ * 页面启动时（挂载时)，加载保存的关卡 ####
+ */
+onMounted(() => {
+  if (props.levelKey) {
+    // 有levelKey说明url本身具有，用户可能是从关卡页面跳转过来的，不用跳转，保存起来即可
+    // 页面刚刚挂载时不会触发watch里的事件，只能在这里解决
+    saveLevel(props.levelKey);
+  } else {
+    // 没有levelKey说明用户可能是在别的页面点击学习按钮或从index跳转过来的，加载之前的关卡
+    // 如果用户已经在学习页面了，再次点击学习按钮，不会触发mounted事件，会触发下面的watch事件
+    loadSaveLevelKey();
+  }
+});
 
 /**
  * 切换关卡时，重置状态
  */
 watch([level], () => {
+  //保存关卡 ####
+  if (props.levelKey) {
+    saveLevel(props.levelKey);
+  }
+  //如果用户已经在学习页面了，再次点击学习按钮，会跳转到/learn，会触发这个的watch事件
+  //（因为上面computed中的props.levelKey发生改变，而且页面没有重载不会触发onMounted）
+  //但是如果用户在level1，再次点击学习按钮，会跳转到/learn，不会触发这个的watch事件，不过反正都是level1，没影响
+  //（因为虽然上面computed中的props.levelKey发生改变，computed重新运行，但是运行结果level的值并未改变，所以不会触发此watch事件）
+  //所以如果props.levelKey变成了空的话，需要加载之前的关卡
+  if (props.levelKey == "") {
+    loadSaveLevelKey();
+  }
+
   activeKeys.value = [...defaultActiveKeys];
 });
 
--- src/pages/LevelsPage.vue
+++ src/pages/LevelsPage.vue
@@ -1,4 +1,9 @@
 <template>
+  <div v-if="saveLevelKey" class="saveLevelKey">
+    <a-button type="primary" @click="clearSaveLevelKey()">
+      清空历史记录
+    </a-button>
+  </div>
   <div id="levelsPage">
     <a-row :gutter="[16, 16]">
       <a-col :md="12" :xs="24">
@@ -7,6 +12,9 @@
             <template #renderItem="{ item: level, index }">
               <a-list-item>
                 <a-list-item-meta :title="`${index + 1}、${level.title}`" />
+                <a-tag v-if="level.key == saveLevelKey" color="blue">
+                  上次学到
+                </a-tag>
                 <template #actions>
                   <router-link :to="`/learn/${level.key}`">
                     <a-button>挑战</a-button>
@@ -23,6 +31,9 @@
             <template #renderItem="{ item: level }">
               <a-list-item>
                 <a-list-item-meta :title="level.title" />
+                <a-tag v-if="level.key == saveLevelKey" color="blue">
+                  上次学到
+                </a-tag>
                 <template #actions>
                   <router-link :to="`/learn/${level.key}`">
                     <a-button>挑战</a-button>
@@ -40,6 +51,36 @@
 <script setup lang="ts">
 import mainLevels from "../levels/mainLevels";
 import customLevels from "../levels/customLevels";
+import { onMounted, ref } from "vue";
+import { message } from "ant-design-vue";
+import { readLevel, deleteLevel } from "../core/saveLevel";
+
+const saveLevelKey = ref();
+
+// 打开页面时加载保存的levelKey
+onMounted(() => {
+  loadSaveLevelKey();
+});
+
+// 加载缓存关卡
+const loadSaveLevelKey = () => {
+  const saveLevelKeyStorage = readLevel();
+  if (!saveLevelKeyStorage) {
+    saveLevelKey.value = "";
+    return;
+  }
+  saveLevelKey.value = saveLevelKeyStorage;
+};
+
+const clearSaveLevelKey = () => {
+  deleteLevel();
+  loadSaveLevelKey();
+  message.info(`清除成功！`);
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.saveLevelKey {
+  margin-bottom: 6px;
+}
+</style>
